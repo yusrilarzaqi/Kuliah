36a9116a36ce7b3f015fa060b77c07d9dc9dd48b
yusrilarzaqi <arzaqiyusril@gmail.com>
2022-12-12 09:48:43 +0700

    add new materi pertemuan 12 Pemrograman Berbasis Object

---
+++ Semester 3/Pemrograman Berbasis Object/pertemuan 12/src/Input_form.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Input_form {
+  public static void main(String[] args) {
+    // Deklarasi variable
+    String nama, alamat;
+    int usia, gaji;
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Pendataan Karyawan PT.XYZ");
+    
+    // input user dimasukan ke variable
+    System.out.print("Nama Karyawan : ");
+    nama = input.nextLine();
+    System.out.print("Alamat : ");
+    alamat = input.nextLine();
+    System.out.print("Usia");
+    usia = input.nextLine();
+    System.out.print("Gaji : ");
+    gaji = input.nextLine();
+
+    // Menampilkan apa yang sudah disimpan di variable
+    System.out.println("Nama Karyawan : " + nama);
+    System.out.println("Alamat : " + alamat); 
+    System.out.println("Usia : " + usia + " tahun");
+    System.out.println("Gaji : Rp " + gaji);
+  }
+}
